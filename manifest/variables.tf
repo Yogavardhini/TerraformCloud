79673278053eb682823aa7282a7d1eb43d191c6e
Yogavardhini Sivakumar <yogavardhini.sivakumar@valuemomentum.com>
2023-08-07 10:57:53 +0530

    ec2

---
+++ manifest/variables.tf
@@ -0,0 +1,9 @@
+variable "json_file_input" {
+  type        = string
+  default = "C:/Users/tarunku/Music/aws/json_file_input.json"
+}
+variable "json_file" {
+  type        = string
+  default     = "C:/TC/TerraformCloud/manifest/aws_auth.json"
+  description = "JSON file path "
+}
